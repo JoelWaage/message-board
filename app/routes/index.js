eb44ef3e920e88135bb12aa1e9f7ceba47bf4059
Joel Waage <joeltwaage@gmail.com>
2016-09-02 12:29:21 -0700

    add ability to save e new questions to the database and display them on the index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
+
+  actions: {
+    save3(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newPost.save();
+      this.transitionTo('index');
+    }
+  }
 });
