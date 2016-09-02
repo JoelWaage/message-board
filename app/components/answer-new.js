a5831cdf4fbffe60e3a7748763f73cf1465607bd
Joel Waage <joeltwaage@gmail.com>
2016-09-02 15:55:58 -0700

    add ability to save answers to database

--- app/components/answer-new.js
+++ app/components/answer-new.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+
+    save4() {
+      var params = {
+        name: this.get("name"),
+        ans: this.get("ans"),
+        question: this.get('question')
+      };
+      this.set('addNewAsnwer', false);
+      this.sendAction('save5', params);
+    }
+  }
 });
