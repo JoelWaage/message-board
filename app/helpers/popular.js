9a05164eb19118ff67c6d1e48049f1fa1a4d294b
Joel <joeltwaage@gmail.com>
2016-09-12 13:51:14 -0700

    add final else statement to helper to show message for questions that have more than five answers

--- app/helpers/popular.js
+++ app/helpers/popular.js
@@ -3,9 +3,11 @@ import Ember from 'ember';
 export function popular(params/*, hash*/) {
   var answer = params[0];
   if (answer.get('answers').get('length') === 0) {
-    return Ember.String.htmlSafe('<span class="zero-response">(awaiting response)</span>');
+    return Ember.String.htmlSafe('<span class="zero-response">(awaiting answer)</span>');
   } else if (answer.get('answers').get('length') <= 5) {
     return Ember.String.htmlSafe('<span class="some-response">(looking for a few more responses)</span>');
+  } else {
+    return Ember.String.htmlSafe('<span class="many-response">(well-ansered, but feel free to contribute more)</span>');
   }
 }
 
