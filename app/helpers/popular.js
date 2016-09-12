fc0a93ece307080f975b32a91b6593f896a41e08
Joel <joeltwaage@gmail.com>
2016-09-12 13:48:20 -0700

    add else statement to helper to show message when a question has fewer than five answers

--- app/helpers/popular.js
+++ app/helpers/popular.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export function popular(params/*, hash*/) {
   var answer = params[0];
   if (answer.get('answers').get('length') === 0) {
-    return Ember.String.htmlSafe('<span class="popular-response">(awaiting response)</span>');
+    return Ember.String.htmlSafe('<span class="zero-response">(awaiting response)</span>');
+  } else if (answer.get('answers').get('length') <= 5) {
+    return Ember.String.htmlSafe('<span class="some-response">(looking for a few more responses)</span>');
   }
 }
 
