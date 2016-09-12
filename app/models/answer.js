734e79787d51d1f6fdea4a588eb0d6da188535e6
Joel <joeltwaage@gmail.com>
2016-09-12 14:34:59 -0700

    add computed property to concat name and answer in the answer model

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,7 +1,12 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   name: DS.attr(),
   ans: DS.attr(),
-  question: DS.belongsTo('question', { async: true })
+  question: DS.belongsTo('question', { async: true }),
+
+  answerSentence: Ember.computed(function() {
+    return this.get('name') + ' thinks ' + this.get('ans') + '.';
+  })
 });
